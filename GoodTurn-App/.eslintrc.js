40a27519e762665cd39f5cc99b2379ef34f95e53
craig1123 <craigwalker1123@gmail.com>
2017-04-24 18:26:29 -0600

    trying to get eslint working

---
+++ GoodTurn-App/.eslintrc.js
@@ -0,0 +1,17 @@
+// module.exports = {
+//   extends: 'airbnb',
+//   parser: 'babel-eslint',
+//   env: {
+//     browser: true
+//   },
+//   parserOptions: {
+//     ecmaFeatures: {
+//       experimentalObjectRestSpread: true
+//     },
+//   },
+//   rules: {
+//     'eol-last': 'off',
+//     'import/no-extraneous-dependencies': 'off',
+//     'import/newline-after-import': 'off',
+//   },
+// };
